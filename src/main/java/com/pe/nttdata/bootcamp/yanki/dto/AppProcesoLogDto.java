3db16ee44b76d490ee549c87e1615b3da032bf10
Hugo Oliveros <hugo.oliveros@gmail.com>
2024-03-19 16:46:42 -0500

    Cambios test

--- src/main/java/com/pe/nttdata/bootcamp/yanki/dto/AppProcesoLogDto.java
+++ src/main/java/com/pe/nttdata/bootcamp/yanki/dto/AppProcesoLogDto.java
@@ -1,9 +1,28 @@
 package com.pe.nttdata.bootcamp.yanki.dto;
 
-import lombok.*;
-
 import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+/**
+ *<b>Class</b>: {@link CurrencyWalletDto}<br/>
+ *<b>Copyright</b>: &Copy; 2024 NTTDATA Per&uacute;. <br/>
+ *<b>Company</b>: NTTDATA del Per&uacute;. <br/>
+ *
+ *@author NTTDATA Per&uacute;. (EVE) <br/>
+ *<u>Developed by</u>: <br/>
+ *<ul>
+ *<li>Hugo Oliveros Monti</li>
+ *</ul>
+ *<u>Changes</u>:<br/>
+ *<ul>
+ *<li>feb. 29, 2024 (acronym) Creation class.</li>
+ *</ul>
+ *@version 1.0
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,15 +30,15 @@ import java.util.Date;
 @ToString
 public class AppProcesoLogDto {
 
-    private Date fechaInicioEjecucion;
-    private Date fechaFinEjecucion;
-    private String estadoEjecucion;
-    private String descripcionEstadoEjecucion;
-    private String modulo;
-    private String claseProgramacion;
-    private String metodoProgramacion;
-    private String parametroEntrada;
-    private String resultadoSalida;
-    private Integer duracionMs;
+  private Date fechaInicioEjecucion;
+  private Date fechaFinEjecucion;
+  private String estadoEjecucion;
+  private String descripcionEstadoEjecucion;
+  private String modulo;
+  private String claseProgramacion;
+  private String metodoProgramacion;
+  private String parametroEntrada;
+  private String resultadoSalida;
+  private Integer duracionMs;
 
 }
